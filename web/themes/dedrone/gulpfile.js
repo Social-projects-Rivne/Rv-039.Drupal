4bf315f4850cabc090da11409bbef4af420977b8
dimidjy <dimidjy322@gmail.com>
2018-08-06 18:28:31 +0300

    Merge gulp and basic config

---
+++ web/themes/dedrone/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+
+
+gulp.task('styles', function(){
+  gulp.src(['scss/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('css/'))
+});
+
+
+gulp.task('default', function(){
+  gulp.watch("scss/**/*.scss", ['styles']);
+});
